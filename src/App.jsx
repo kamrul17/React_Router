f9161925af06c01923242572f6cb801778fb61cc
kamrul17 <kamrula786@gmail.com>
2025-04-03 21:54:40 +0530

    added lazy in routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Contact from "./Contact";
-import About from "./About";
+import { Suspense } from "react";
+// import About from "./About";
 import NavigatedExmPage from "./NavigatedExmPage";
 
+const About = React.lazy(() => import("./About"));
 function App() {
   return (
     <>
@@ -14,12 +16,16 @@ function App() {
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
       </nav>
-      <Routes>
-        <Route path="/" element={<div>i m home page</div>} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/naviPage" element={<NavigatedExmPage />} />
-      </Routes>
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<div>i m home page</div>} />
+
+          <Route path="/about" element={<About />} />
+
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/naviPage" element={<NavigatedExmPage />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
